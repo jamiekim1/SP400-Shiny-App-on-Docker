8342d2f01fb80429ef8321d2f635302f4705597d
jamiekim1 <jamiek101699@gmail.com>
2024-02-08 22:44:17 -0700

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -1,8 +1,10 @@
-p <- c("tidyquant", "plotly", "devtools", "tidyverse", "shiny", "ggplot2")
+p <- c("tidyquant", "plotly", "devtools", "tidyverse", "shiny", "ggplot2",
+       "learnr", "magrittr", "rlang", "lifecycle", "glue")
 
 new.packages <- p[!(p %in% installed.packages()[, "Package"])]
 if (length(new.packages)) {
   install.packages(new.packages, dependencies = TRUE)
 }
 
+devtools::install_github("risktoollib/RTL")
 devtools::install_github("risktoollib/RTLedu")
