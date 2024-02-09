45e1d336ee975ada8b43c1019c356704ef1a2341
jamiekim1 <jamiek101699@gmail.com>
2024-02-08 22:39:39 -0700

    shinyapp update

--- requirements.R
+++ requirements.R
@@ -1,5 +1,8 @@
-p <- c("tidyquant","plotly","RTLedu","tidyverse", "shiny", "ggplot2")
+p <- c("tidyquant", "plotly", "devtools", "tidyverse", "shiny", "ggplot2")
+
 new.packages <- p[!(p %in% installed.packages()[, "Package"])]
 if (length(new.packages)) {
   install.packages(new.packages, dependencies = TRUE)
 }
+
+devtools::install_github("risktoollib/RTLedu")
